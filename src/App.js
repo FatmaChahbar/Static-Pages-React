1773a9a660a1d827092ce74d25cf48224217dc83
FatmaChahbar <fatma.chahbar@gmail.com>
2022-12-13 17:13:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <header>
+<nav>
+  <img width={50} src={logo} />
+  
+
+  <ul >
+    <li>Pricing </li>
+    <li>About</li>
+    <li>Contact</li>
+  </ul>
+</nav>
+</header>
+<h1> the Reasens that I Prefer Learning React</h1>
+      <ol>
+        <li>I'm more likely to get a job as developer if I know React</li>
+        <li>I found it so easy and I enjoy Learning it </li>
+        <li>Try New Things like developing Web Pages , and it's popular library</li>
+      </ol>
+      <footer > <small > @ Fatma CHAHBAR development, all rightd reserved.</small></footer>
+
+
+      
     </div>
   );
 }
